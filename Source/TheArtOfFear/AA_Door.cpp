79e9507bb6de43dcdc3416b71b5355f6f7b08c95
Luke2104 <115215930+Luke2104@users.noreply.github.com>
2024-03-25 13:12:11 +0000

    DoorStart

---
+++ Source/TheArtOfFear/AA_Door.cpp
@@ -0,0 +1,19 @@
+// Produced by Lucky 13 (Team 13) for module GDEV60033, Staffordshire University.
+
+
+#include "AA_Door.h"
+
+
+AAA_Door::AAA_Door()
+{
+	
+
+}
+
+
+void AAA_Door::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
