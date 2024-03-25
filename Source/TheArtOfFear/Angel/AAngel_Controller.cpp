8f6447722a163cf28b38bb34a1439e0cc96269b4
Jakelmoss <120012521+ECarlow@users.noreply.github.com>
2024-03-25 13:57:47 +0000

    Added Main Menu UI + Fixed Angels
    - Added folder containing UI functionality for Main Menu
    - Fixed a bug which caused the Angel to crash the project
    - Changed game default map to main menu level

--- Source/TheArtOfFear/Angel/AAngel_Controller.cpp
+++ Source/TheArtOfFear/Angel/AAngel_Controller.cpp
@@ -3,3 +3,38 @@
 
 #include "AAngel_Controller.h"
 
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+
+
+AAAngel_Controller::AAAngel_Controller()
+{
+	BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent>(TEXT("Behavior Tree Component"));
+	BlackboardComponent = CreateDefaultSubobject<UBlackboardComponent>(TEXT("Blackboard Component"));
+}
+
+void AAAngel_Controller::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (IsValid(BehaviorTree.Get()))
+	{
+		RunBehaviorTree(BehaviorTree.Get());
+		BehaviorTreeComponent->StartTree(*BehaviorTree.Get());
+	}
+}
+
+
+void AAAngel_Controller::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	if (IsValid(Blackboard.Get()) && IsValid(BehaviorTree.Get()))
+	{
+		Blackboard->InitializeBlackboard(*BehaviorTree.Get()->BlackboardAsset.Get());
+		//Blackboard->SetValueAsObject("PlayerCharacter", UGameplayStatics::GetPlayerCharacter(GetWorld(),0));
+	}
+}
--- Source/TheArtOfFear/Angel/AAngel_Controller.h
+++ Source/TheArtOfFear/Angel/AAngel_Controller.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
 #include "AAngel_Controller.generated.h"
 
 /**
@@ -13,5 +14,19 @@ UCLASS()
 class THEARTOFFEAR_API AAAngel_Controller : public AAIController
 {
 	GENERATED_BODY()
+public:
+	AAAngel_Controller();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnPossess(APawn* InPawn) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI", meta = (AllowPrivateAccess = true))
+	TObjectPtr<UBehaviorTree> BehaviorTree;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI", meta = (AllowPrivateAccess = true))
+	TObjectPtr<UBehaviorTreeComponent> BehaviorTreeComponent;
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI", meta = (AllowPrivateAccess = true))
+	TObjectPtr<UBlackboardComponent> BlackboardComponent;
 };
