085ca2501f721260945d6b79cda6c1402ca42824
Tommy Eaves <teaves512@users.noreply.github.com>
2024-05-12 19:13:08 +0100

    UAPhotoGallery base class

---
+++ Source/TheArtOfFear/DigitalCamera/APhotoGallery.h
@@ -0,0 +1,18 @@
+// Produced by Lucky 13 (Team 13) for module GDEV60033, Staffordshire University.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "APhotoGallery.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEARTOFFEAR_API UAPhotoGallery : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
