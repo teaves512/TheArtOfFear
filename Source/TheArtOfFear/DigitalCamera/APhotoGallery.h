9e369819b3c38c975d310520143ec09ac0ec3077
Tommy Eaves <teaves512@users.noreply.github.com>
2024-05-13 00:14:20 +0100

    UAPhotoGallery base class

--- Source/TheArtOfFear/DigitalCamera/APhotoGallery.h
+++ Source/TheArtOfFear/DigitalCamera/APhotoGallery.h
@@ -7,12 +7,23 @@
 
 #include "APhotoGallery.generated.h"
 
+class UTextureRenderTarget2D;
+
 /**
- * 
+ * Base class for WBP_PhotoGallery. Manages a number of screen captures
+ * and provides the player with a method of cycling between them.
  */
 UCLASS()
 class THEARTOFFEAR_API UAPhotoGallery : public UUserWidget
 {
 	GENERATED_BODY()
+
+	// INTERFACE
+public:
+	void SetPhotoRender(UTextureRenderTarget2D* InPhotoRender);
+
+	// INTERFACE
+protected:
+	//...
 	
 };
