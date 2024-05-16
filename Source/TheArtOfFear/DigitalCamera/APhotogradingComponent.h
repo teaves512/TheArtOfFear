778b4030390fb4c7d2f38449c48fe17538be5b51
Tommy Eaves <teaves512@users.noreply.github.com>
2024-05-16 18:39:03 +0100

    APhotogradingComponent Cpp Base Class

---
+++ Source/TheArtOfFear/DigitalCamera/APhotogradingComponent.h
@@ -0,0 +1,20 @@
+// Produced by Lucky 13 (Team 13) for module GDEV60033, Staffordshire University.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+
+#include "APhotogradingComponent.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable )
+class THEARTOFFEAR_API UAPhotogradingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+	// INTERFACE
+public:
+	// UFUNCTION(BlueprintImplementableEvent, )
+	// int32 GradePhoto(FVector EyeLocation, APlayerController* PlayerController);
+	
+};
